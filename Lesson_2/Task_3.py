f477f1635a1c2e5d647cbd73b46716616e7299ed
Alexander <xanderx2201@yandex.ru>
2022-12-13 00:38:50 +0300

    Lesson 2 complete

--- Lesson_2/Task_3.py
+++ Lesson_2/Task_3.py
@@ -2,3 +2,12 @@
 #     *Пример:*
 #     - Для n = 3:  {1: 2, 2: 2.25 , 3: 2.37}
 #     Необходимо сложить все значения словаря и вывести  сумму на экран.
+
+number = int(input('Введите целое число n= '))
+number_list = {}
+sum = 0
+for i in range(1, number + 1):
+    number_list[i] = round((1+(1/i)) ** i, 3)
+    sum += number_list[i]
+print(number_list)
+print(f'Сумма значений = {round(sum,3)}')
--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -1,3 +1,42 @@
-# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
-# Найдите произведение элементов на указанных позициях. 
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
+
+#Этот вариант почему-то у меня не заработал
+
+# n = int(input('Введите число n= '))
+# list_n = []
+# for i in range(-abs(n), abs(n) + 1):
+#     list_n.append(i)
+# print(list_n)
+
+# prod = 1
+# path = 'file.txt'
+# with open(path, 'r') as data:
+#     for line in data:
+#         prod *= list_n[int(line)]
+#     print(prod)
+#---------------------------------------------------
+
+def find_product_ininterval(n, m, p):
+
+
+    if m > 2*n or p > 2*n:
+        print("index cant be more than interval")
+        exit()
+
+    interval_nminusn = range(-n, n + 1)
+
+    data_write = open('file.txt', 'w')
+    for i in interval_nminusn:
+        data_write.write(str(i) + '\n')
+    data_write.close()
+
+    data_read = open('file.txt', 'r')
+    string_of_interval = (data_read.read()).split()
+    data_read.close()
+
+    print(f"{string_of_interval[m]}(index{m}){string_of_interval[p]}(index{p})={int(string_of_interval[m]) + int(string_of_interval[p])}")
+
+
+find_product_ininterval(5, 7, 4)
