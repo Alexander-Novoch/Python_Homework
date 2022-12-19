1ba442d6377bbae7ab7f05955c4a63db65dfc3b4
Alexander <xanderx2201@yandex.ru>
2022-12-19 18:23:26 +0300

    Lesson 3 complete

--- Lesson_3/Task_4.py
+++ Lesson_3/Task_4.py
@@ -1,7 +1,15 @@
 # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-
 # Пример:
-
 #     - 45 -> 101101
 #     - 3 -> 11
 #     - 2 -> 10
+
+def dec_to_bin(num):
+    bin_num = ''
+    while num>0:
+        bin_num = f'{num%2}{bin_num}'
+        num = num//2
+    return bin_num
+
+dec_num = int(input('Введите число: '))
+print(dec_to_bin(dec_num))
--- Lesson_3/Task_5.py
+++ Lesson_3/Task_5.py
@@ -1,11 +1,32 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
-
 # Пример:
-
-#     - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
+#     - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 # [Негафибоначчи](https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8#:~
 # :text=%D0%92%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B5%2C%20%D1%87%D0%B8%D1%81%D0%BB%D0%B0%20%D0%BD%D0%B5%
 # D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8%20%E2%80%94%20%D0%BE%D1%82%D1%80%D0%B8%D1%86%D0%B0%D1%82%D0%B5
 # %D0%BB%D1%8C%D0%BD%D0%BE%20%D0%B8%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%8D%D0
 # %BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D1%8B%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%B
 # E%D1%81%D1%82%D0%B8%20%D1%87%D0%B8%D1%81%D0%B5%D0%BB%20%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8.)
+input_number = int(input('Введите целое число: '))
+
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+    return fib(n-1)+fib(n-2)
+
+
+def reverse_fib(n):
+    return (-1)**(n+1)*fib(n)
+
+
+fib_list = []
+for i in range(input_number + 1):
+    fib_list.append(reverse_fib(i))
+
+fib_list.reverse()
+fib_list.pop(-1)
+
+for i in range(input_number + 1):
+    fib_list.append(fib(i))
+
+print(fib_list)
